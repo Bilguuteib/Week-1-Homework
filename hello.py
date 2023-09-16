40b2b26951cf669400793fe1a99a1e6ec0006069
Bilguuteib <Bilguutei.batbayar@gmail.com>
2023-09-16 13:40:02 +0800

    test 2

---
+++ hello.py
@@ -0,0 +1,18 @@
+msg = "Roll a dice"
+print(msg)
+9+15
+
+import numpy as np
+
+msg = "Roll a dice"
+print(msg)
+
+print(np.random.randint(1,9))
+
+# Testing
+msg = "I love you, Aminaa"
+print(msg)
+
+125+3215
+
+Testing 2
